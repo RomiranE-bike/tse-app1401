0c375d797195fedf84ac10733253622abdd8b664
mahmoud esmaeili <99283119+mesm1749@users.noreply.github.com>
2022-02-13 22:07:43 +0330

    Update fipiran_data.py

--- fipiran_data.py
+++ fipiran_data.py
@@ -92,16 +92,6 @@ fara_bourse_status=dict(
 # print(bourse_status)
 # print(fara_bourse_status)
 # --------------------------------------------------------------------------------
-import os
-from winsound import Beep
-import time
-import datetime
 
-# time.sleep(1)
-Beep(2000,100)
-print('fipiran_data:executed.')
-
-t = datetime.datetime.now()
-print(t.minute,':', t.second,':',str(t.microsecond)[:2])
 
 
